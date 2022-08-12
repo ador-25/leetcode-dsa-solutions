89497f68c0aaf41fb5da406ae5acc7c02865fee1
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2022-08-13 01:19:06 +0600

    Time: 0 ms (100.00%), Space: 46.1 MB (18.61%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int result[]= new int[nums.length];
+        int result[]= new int[n*2];
         int slowPointer=0;
         for(int i=0;i<result.length;i+=2){
             result[i]=nums[slowPointer];
