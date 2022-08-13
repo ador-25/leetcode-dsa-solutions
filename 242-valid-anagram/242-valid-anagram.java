3d52375a5e85c0f3d6e31a5b2b483099e6e0da31
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2022-08-13 22:36:54 +0600

    Time: 4 ms (84.45%), Space: 43.4 MB (71.92%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -3,16 +3,16 @@ class Solution {
         if(s.length()!=t.length())
             return false;
         int[]map= new int[256];
+        int sum=0;
         for(int i=0;i<s.length();i++){
             map[(int)s.charAt(i)]++;
+            sum++;
         }
         for(int i=0;i<t.length();i++){
             if(--map[(int)t.charAt(i)]<0)
                 return false;
+            sum--;
         }
-        for(int i=0;i<map.length;i++)
-            if(map[i]!=0)
-                return false;
-        return true;
+        return sum==0;
     }
 }
