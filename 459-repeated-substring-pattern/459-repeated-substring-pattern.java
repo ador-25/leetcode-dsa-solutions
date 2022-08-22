234d4c11fd59b1940a9eddde626eefe1bec51931
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2022-08-22 11:13:03 +0600

    Time: 8 ms (97.50%), Space: 50.7 MB (50.22%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -1,10 +1,24 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        //without loop approach
-        String newString= (s+s).substring(1,s.length()*2-1);
-        if(newString.contains(s))
-            return true;
-        return false;
-
+        //kmp approach
+        char[] pattern = s.toCharArray();
+        int n= s.length();
+        int []failures= new int[n];
+        int fastRunner=1,slowRunner=0;
+        while(fastRunner<n){
+         if(pattern[fastRunner]==pattern[slowRunner]){
+             failures[fastRunner++]= ++slowRunner;
+         }
+         else if(slowRunner>0){
+             slowRunner=failures[slowRunner-1];
+         }
+         else{
+             fastRunner++;
+         }
+        }
+        if(failures[n-1]==0)
+            return false;
+        else return(failures[n-1]%(n-failures[n-1])==0);
+        
     }
 }
