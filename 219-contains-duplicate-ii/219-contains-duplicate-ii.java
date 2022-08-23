4851a4a3c7ecbeca8d22055480576a5b83eccfe9
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2022-08-23 10:04:11 +0600

    Time: 40 ms (58.30%), Space: 95.6 MB (17.75%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             int temp= map.getOrDefault(nums[i],-1);
             if(temp!=-1){
-                if(Math.abs(i-temp)<=k)
+                if(i-temp<=k)
                     return true;
             }
             map.put(nums[i],i);
