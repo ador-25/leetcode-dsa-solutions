2ab1e9bbf63294d7c42354b4906a970ab9d90288
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2022-09-13 09:29:26 +0600

    Time: 0 ms (100.00%), Space: 43 MB (77.91%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -2,15 +2,13 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int left=0,right=nums.length-1;
         while (left<=right){
-            int pivot= left+right >>>1;
-            if(nums[pivot]>target){
-                right=pivot-1;
-            }
-            else if(nums[pivot]<target){
-                left=pivot+1;
-            }
-            else
+            int pivot= (left+right)>>>1;
+            if(nums[pivot]==target)
                 return pivot;
+            else if(nums[pivot]>target)
+                right=pivot-1;
+            else
+                left=pivot+1;
         }
         return left;
     }
