4de2583232e781dd85f387fa550e0197ee637903
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2022-09-13 09:36:23 +0600

    Time: 27 ms (21.81%), Space: 70.9 MB (32.38%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>(nums.length);
         for(int item:nums)
             if(set.contains(item))
                 return true;
             else
                 set.add(item);
-        return false;    
+        return false;
     }
 }
