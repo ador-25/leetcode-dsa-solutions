d479d2e70bc2387bf7a9d2ddf7dbf1a1ab78e7b5
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2022-12-08 01:00:39 +0600

    Time: 41 ms (88.71%), Space: 137.4 MB (87.44%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -20,13 +20,11 @@ class Solution {
         }
     }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-       // if(edges.length==0)return false;
 
         UnionFind u=new UnionFind(n);
         for(int i=0;i<edges.length;i++){
             u.union(edges[i][0],edges[i][1]);
         }
-        System.out.println(u.find(source)+" "+u.find(destination));
         return u.find(source)==u.find(destination);
     }
 }
