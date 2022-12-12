ee0d36f28e4d4e49aeec3b42550b4f69197ec3dc
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2022-12-13 05:05:15 +0600

    Time: 1 ms (91.76%), Space: 44 MB (43.82%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.java
+++ 684-redundant-connection/684-redundant-connection.java
@@ -1,14 +1,6 @@
 class Solution {
     static class UnionFind{
         int []arr;
-        public UnionFind(char [][]c){
-            int n= c.length*c[0].length;
-            arr= new int[n+1];
-
-            for(int i=0;i<n+1;i++){
-                arr[i]=i;
-            }
-        }
         public UnionFind(int n){
             arr=new int[n+1];
             for (int i=1;i<arr.length;i++){
@@ -20,12 +12,6 @@ class Solution {
                 return x;
             else return find(arr[x]);
         }
-        public void union(int a, int b){
-            int x=find(a);
-            int y=find(b);
-            if(x==y)return;
-            else arr[y]=x;
-        }
         public boolean makeFriend(int a, int b){
             int x=find(a);
             int y=find(b);
@@ -34,13 +20,7 @@ class Solution {
             return true;
             
         }
-        //graphs in forest
-        public int numberOfProvinces(){
-            HashSet<Integer> set= new HashSet<>();
-            for(int i=1;i<arr.length;i++)
-                set.add(find(arr[i]));
-            return set.size();
-        }
+
     }
     public int[] findRedundantConnection(int[][] edges) {
         UnionFind u= new UnionFind(edges.length);
