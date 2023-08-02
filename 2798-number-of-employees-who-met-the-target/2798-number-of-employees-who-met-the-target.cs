444971d236ff36eda387fd8bc819de6fc562869f
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2023-08-02 17:18:58 +0600

    Time: 80 ms (53.51%), Space: 40.4 MB (93.86%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cs
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.cs
@@ -1,9 +1,14 @@
 public class Solution {
-    public int NumberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int count =0;
-        for(int i=0;i<hours.Length;i++)
-            if(hours[i]>=target)
-                count++;
-        return count;
+public int NumberOfEmployeesWhoMetTarget(ReadOnlySpan<int> hours, int target)
+{
+    int count = 0;
+    for (int i = 0; i < hours.Length; i++)
+    {
+        if (hours[i] >= target)
+        {
+            count++;
+        }
     }
+    return count;
+}
 }
