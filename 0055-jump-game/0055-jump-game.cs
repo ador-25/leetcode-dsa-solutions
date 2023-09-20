2376c9c1ee377500c279ce88a71d21d1de418883
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2023-09-20 13:01:37 +0600

    Time: 148 ms (63.51%), Space: 62.2 MB (42.38%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool CanJump(int[] nums)
+    {
+
+        int nearestEscapeIndex = nums.Length - 1;
+        for (int i = nums.Length-2; i >= 0; i--)
+        {
+            if (i + nums[i] >= nearestEscapeIndex)
+                nearestEscapeIndex = i;
+        }
+        
+        if(nearestEscapeIndex ==0)
+            return true;
+        
+        for (int i = 0; i < nums.Length-1; i++)
+        {
+            if (i + nums[i] >= nearestEscapeIndex)
+                return true;
+            //cant move forward
+            if(nums[i]==0)
+                break;
+        }
+        return false;
+    }
+}
