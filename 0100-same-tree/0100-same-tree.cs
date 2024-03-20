f76f4f353b7c59710c5583fec7546977fb77573b
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2024-03-21 03:43:44 +0600

    Time: 80 ms (5.49%), Space: 42 MB (91.09%) - LeetHub

--- 0100-same-tree/0100-same-tree.cs
+++ 0100-same-tree/0100-same-tree.cs
@@ -14,16 +14,13 @@
 public class Solution {
     public bool IsSameTree(TreeNode p, TreeNode q) 
     {
-        return isSame(p,q);
-    }
-    private bool isSame(TreeNode p, TreeNode q)
-    {
         if(p==null && q==null)
             return true;
         if(p==null || q==null)
             return false;
         if(p.val != q.val)
             return false;
-        return isSame(p.left,q.left) && isSame(p.right,q.right);
+        return IsSameTree(p.left,q.left) && IsSameTree(p.right,q.right);
     }
+
 }
