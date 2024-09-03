0736851b8263b8cfaa174ed94e0eab10d0ea46e1
Abtahee Farhan Ador <84260545+Ador-25@users.noreply.github.com>
2024-09-03 22:03:50 +0600

    Time: 271 ms (41.35%), Space: 67.6 MB (31.78%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -5,10 +5,7 @@ class Solution
         HashSet<int> set = new HashSet<int>();
         for (int i = 0; i < nums.Length; i++)
         {
-            if (set.Contains(nums[i]))
-            {
-                return true;
-            }
+            if (set.Contains(nums[i])) return true;
             set.Add(nums[i]);
         }
         return false;
